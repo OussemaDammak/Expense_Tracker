d3b4e90ca54d9955e45cf42892404fdaadccda25
oussema <oussemadammak01@gmail.com>
2024-02-20 06:48:28 +0100

    add formulaire+modal sheet

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -33,21 +34,29 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title:const Text('Flutter ExpenseTracker'), 
+        title: const Text('Flutter ExpenseTracker'),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
         ],
-
       ),
-
       body: Column(
         children: [
           const Text('Chart'),
-          Expanded(child:ExpensesList(expenses: _registredExpenses)),
+          Expanded(child: ExpensesList(expenses: _registredExpenses)),
         ],
       ),
     );
