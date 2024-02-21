2343ba64308fed94b043ce4b648b07201447cb3c
oussema <oussemadammak01@gmail.com>
2024-02-21 18:47:30 +0100

    create chart Model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -28,7 +28,34 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  String get formatedDate{
+  String get formatedDate {
     return formatter.format(date);
+  }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where(
+              (expense) => expense.category == category,
+            )
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
 }
