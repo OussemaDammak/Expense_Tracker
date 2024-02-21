cb7484a472a8d2adb3fc355d9744075af3826ea2
oussema <oussemadammak01@gmail.com>
2024-02-21 12:04:06 +0100

    use theme color

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 11, 22, 129),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor:kColorScheme.onPrimaryContainer ,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
